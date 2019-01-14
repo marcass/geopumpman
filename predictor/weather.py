55d9873bbebd44bd8a7392615768abdf9dd9ad03
Marcus <best.nose@gmail.com>
2019-01-14 13:13:06 +1300

    using openweathermap

--- predictor/weather.py
+++ predictor/weather.py
@@ -6,14 +6,26 @@ owm = pyowm.OWM(creds.key)  # You MUST provide a valid API key
 # Have a pro subscription? Then use:
 # owm = pyowm.OWM(API_key='your-API-key', subscription_type='pro')
 
-# Search for current weather in London (Great Britain)
-observation = owm.weather_at_place('London,GB')
+  # {
+  #   "id": 7910038,
+  #   "name": "Dunedin City",
+  #   "country": "NZ",
+  #   "coord": {
+  #     "lon": 170.243683,
+  #     "lat": -45.640511
+  #   }
+  # },
+
+# observation = owm.weather_at_place('Dunedin City,NZ')
+observation = owm.weather_at_id(7910038)
+
 w = observation.get_weather()
 print(w)                      # <Weather - reference time=2013-12-18 09:20,
                               # status=Clouds>
 
 # Weather details
-w.get_wind()                  # {'speed': 4.6, 'deg': 330}
+wind = w.get_wind()                  # {'speed': 4.6, 'deg': 330}
+print(wind)
 w.get_humidity()              # 87
 w.get_temperature('celsius')  # {'temp_max': 10.5, 'temp': 9.7, 'temp_min': 9.0}
 
