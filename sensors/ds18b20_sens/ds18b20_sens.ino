41a367952db708c0f16e78dc06af342a4dd03092
Marcass <best.nose@gmail.com>
2019-10-17 15:29:38 +1300

    sorting data

--- sensors/ds18b20_sens/ds18b20_sens.ino
+++ sensors/ds18b20_sens/ds18b20_sens.ino
@@ -2,7 +2,7 @@
 #include <OneWire.h>
 #include <DallasTemperature.h>
 // Data wire is plugged into pin 2 on the Arduino
-#define ONE_WIRE_BUS 2
+#define ONE_WIRE_BUS A2
 #define EXCHANGER 1 //1/2/3/4
 const String POSITION = "start"; //start || end
 String filename = EXCHANGER+"_"+POSITION+".csv";
@@ -16,8 +16,8 @@ DallasTemperature sensors(&oneWire);
  * http://henrysch.capnfatz.com/henrys-bench/arduino-tempera#define FRIDGE_RELAY 10
 ture-measurements/ds18b20-arduino-user-manual-introduction-and-contents/ds18b20-user-manual-part-2-getting-the-device-address/
  */
-DeviceAddress TOP_TEMP_SENSOR = {0x28, 0xFF, 0x16, 0x8D, 0x87, 0x16, 0x03, 0x50}; //checked - sensor with breadboard wires on it
-DeviceAddress BOTTOM_TEMP_SENSOR = {0x28, 0xCC, 0x02, 0xAF, 0x03, 0x00, 0x00, 0xA8};
+DeviceAddress TOP_TEMP_SENSOR =     {0x28, 0xFF, 0x16, 0x8D, 0x87, 0x16, 0x03, 0x50}; //checked - sensor with breadboard wires on it
+DeviceAddress BOTTOM_TEMP_SENSOR =  {0x28, 0xFF, 0xE8, 0x8F, 0x70, 0x16, 0x05, 0x79}; //checked
 
 
 void setup() {
@@ -26,17 +26,20 @@ void setup() {
   // Start up the library
   // sensors.begin();
   // set the resolution to 9 bit - Valid values are 9, 10, or 11 bit.
-  sensors.setResolution(tempSensor, 9);
+  sensors.setResolution(TOP_TEMP_SENSOR, 11);
+  sensors.setResolution(BOTTOM_TEMP_SENSOR, 11);
   // confirm that we set that resolution by asking the DS18B20 to repeat it back
-  Serial.print("Sensor Resolution: ");
-  Serial.println(sensors.getResolution(tempSensor), DEC);
+  Serial.print("Sensor Resolution: Top - ");
+  Serial.print(sensors.getResolution(TOP_TEMP_SENSOR), DEC);
+  Serial.print(", Bottom - ");
+  Serial.println(sensors.getResolution(BOTTOM_TEMP_SENSOR), DEC);
   Serial.println();
 }
 
 void loop() {
   // call sensors.requestTemperatures() to issue a global temperature
   // request to all devices on the bus
-  Serial.print(" Requesting temperatures...");
+//  Serial.print(" Requesting temperatures...");
   // Tell the Sensor to Measure and Remember the Temperature it Measured
   // http://henrysbench.capnfatz.com/henrys-bench/arduino-temperature-measurements/ds18b20-arduino-user-manual-introduction-and-contents/ds18b20-arduino-users-manual-part-3-reading-a-single-temperature/
   sensors.requestTemperaturesByAddress(TOP_TEMP_SENSOR);// Send the command to get temperatures
@@ -44,19 +47,17 @@ void loop() {
   delay(500);
   sensors.requestTemperaturesByAddress(BOTTOM_TEMP_SENSOR);
   float bottom = sensors.getTempC(BOTTOM_TEMP_SENSOR);
-  Serial.println("DONE");
+//  Serial.println("DONE");
   /********************************************************************/
-  Serial.print("Top temperature is: ");
-  Serial.print(top, 4); // The four just increases the resolution that is printed
-  Serial.print("; Bottom temperature is: ");
-  Serial.println(bottom, 4);
+//  Serial.print("Top temperature is: ");
+//  Serial.print(top, 4); // The four just increases the resolution that is printed
+//  Serial.print("; Bottom temperature is: ");
+//  Serial.println(bottom, 4);
   //Paylod for python3
   Serial.print("DATA/");
   Serial.print(EXCHANGER);
   Serial.print("/");
   Serial.print(POSITION);
-  Serial.print("/");
-  Serial.print(EXCHANGER);
   Serial.print("/");
   Serial.print(top, 4); // The four just increases the resolution that is printed
   Serial.print("/");
--- sensors/python/serial_listener.py
+++ sensors/python/serial_listener.py
@@ -7,15 +7,20 @@ def readlineCR(port):
     rv = ""
     while True:
         ch = port.read()
-#        print(ch)
+        # print(ch)
 #        rv += ch
 #       for python3 need to cast to str
-        rv += str(ch, 'UTF-8')
-#        print(rv)
+        # rv += str(ch, 'UTF-8')
+        try:
+            s = ch.decode("UTF-8")
+            rv += s
+            # print(rv)
+        except:
+            pass
         if ch==b'\r':# or ch=='':
             print(rv)
             if 'DATA' in rv:
-                received = rv[5:]
+                received = rv[6:]
                 print(received)
                 data = received.split('/')
                 exchanger = data[0]
@@ -26,16 +31,22 @@ def readlineCR(port):
                 # current date and time
                 now = datetime.now()
                 timestamp = datetime.timestamp(now)
-                print ('exhanger = '+exchanger+'; temps are: '+top+' '+bottom+'; timestamp = '+timestamp)
+                print(data)
+                print(exchanger)
+                print(position)
+                print(top)
+                print(bottom)
+                print(f_name)
+                # print ('exhanger = '+exchanger+'; temps are: '+str(top)+' '+str(bottom)+'; timestamp = '+timestamp)
                 # https://docs.python.org/3/library/csv.html
-                # with open(f_name, 'w+', newline='') as csvfile:
-                #     fieldnames = ['timestamp', 'top_temp', 'bottom_temp']
-                #     writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-                #     writer.writeheader()
-                #     writer.writerow({'timestamp': timestamp, 'top_temp': top, 'bottom_temp': bottom})
+                with open(f_name, 'w+', newline='') as csvfile:
+                    fieldnames = ['timestamp', 'top_temp', 'bottom_temp']
+                    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+                    writer.writeheader()
+                    writer.writerow({'timestamp': timestamp, 'top_temp': top, 'bottom_temp': bottom})
             return rv
 
-port = serial.Serial("/dev/arduino", baudrate=9600, timeout=3.0)
+port = serial.Serial("/dev/ttyUSB0", baudrate=9600, timeout=3.0)
 #port = serial.Serial("/dev/ttyUSB0", baudrate=9600, timeout=3.0)
 
 if __name__ == "__main__":
